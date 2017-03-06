e3c9604d8a99ca1aa81e2dcc9c6c8ff83461f729
arielkergall <ariel.kergall@hotmail.com>
2017-03-06 13:23:59 -0300

    Doing a minor change.

--- scripts/moving.js
+++ scripts/moving.js
@@ -26,16 +26,27 @@ function moveUp(buttonClicked)
 {
 	//from the button clicked, we search the nearest tr, that is the 'tr' that we are going to move
 	var tr = buttonClicked.closest("tr");
-	//we have to say were we want to move the 'tr', in this case, we are goint to move it one position up, 
+	//this "hide", will hide our tr.
+	tr.hide("slow", function(){
+		//we have to say were we want to move the 'tr', in this case, we are goint to move it one position up, 
 	//ergo, before of the actual position
-	tr.insertBefore(tr.prev());
+		tr.insertBefore(tr.prev());
+		//and now we show our tr again, it adds a litle efect
+		tr.show("slow");
+	});
+	
+	
 }
 
 function moveDown(buttonClicked)
 {
 	//from the button clicked, we search the nearest tr, that is the 'tr' that we are going to move
 	var tr = buttonClicked.closest("tr");
+	tr.hide("slow", function(){
 	//we have to say were we want to move the 'tr', in this case, we are goint to move it one position down, 
 	//ergo, after of the actual position
 	tr.insertAfter(tr.next());
+	tr.show("slow");
+	});
+	
 }
